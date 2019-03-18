974b21264a068e82697eca93ff600c2997728947
cariasp27 <paul.carias@yahoo.com>
2019-03-18 19:30:12 -0400

    Working JS and Semantic for styling

--- assets/javascript/moshpit.js
+++ assets/javascript/moshpit.js
@@ -1,118 +1,178 @@
+////////// GLOBAL VARIABLES //////////
+// for holding name of inputArtist the user input
 var inputArtist;
+// for holding the location the user input 
 var where;
-var blank = "";
+// grabs logo for jQuery manipulation
 var logo = $("#logo");
+// lastFM api key, just out in the open
 var key = "fb84c3a57177226f589349b05b02b444";
 
 
-
+var logovalue = $("#logo").attr("value");
+////////// Function for calling LastFM API //////////
 function lastfm(artist) {
-
   var key = "fb84c3a57177226f589349b05b02b444";
   var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=" + key + "&format=json";
-  
+  ////////// AJAX API call //////////
   $.ajax({
     url: queryURL,
     method: "GET"
     
   }).then(function (response) {
+////////// Response Manipulation //////////
+    // log the specific music brainz id from the response,
+    // assign it to the artid variable and then make another
+    // API call to get artist bio using the specific mbid
     console.log("music brainz:" + response.artist.mbid);
     var artid = response.artist.mbid;
     var queryURLtwo = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&mbid=" + artid + "&api_key=" + key + "&format=json";
     $.ajax({
+////////// 2nd AJAX API call //////////
       url: queryURLtwo,
       method: "GET"
     }).then(function (response) {
+////////// Response Manipulation //////////
+      // log the bio to the console and assign to bio variable for manipulation
       console.log("potential bio:"+response.artist.bio.summary);
       var bio = response.artist.bio.summary;
+      // there will be an anchor tag at the end of the biography linking to the full article,
+      // we therefor lindex the bio for the first < occurance and assign the index number to the variable end
       var end = bio.indexOf("<");
+      // log end
       console.log(end);
-      //bio.substring(0, end);
-      var biop = $("<p>").text(bio.substring(0, end));
-      $("#artist-div").append(biop);
+      // substring bio starting at the beginning and ending at end
+      // this will chop any text after the first "<" occurence in bio
+      // assign chopped bio to chopbio variable
+      var chopbio = bio.substring(0, end);
+
+      console.log(chopbio);
+      // create a paragraph element with the text of chopbio and assign it to the biop variable
+      $("#biog").append(chopbio);
     })
 
   })
 };
-
-function evdbapi(artist, loc) {
-  console.log("evdbapi called, artist=" + artist + ", loc = " + loc);
+////////// Function for calling Eventful API //////////
+function evdbapi(inputArtist, where) {
+  // console log the two user inputs
+  console.log("evdbapi called, inputArtist=" + inputArtist + ", where = " + where);
+  // API key for Eventful
   var app_key = "PW24gq77zLtqXLnT";
+  // object containing the arguments to be passed into the API
   var oArgs = {
     app_key: app_key,
-    q: artist,
-    where: loc,
+    q: inputArtist,
+    where: where,
     "date": "Next week",
     "include": "tags,categories",
-    page_size: 20,
+    page_size: 5,
     sort_order: "popularity",
   };
   EVDB.API.call("/events/search", oArgs, function (oData) {
-    console.log(oData.events);
+////////// Response Manipulation ////////// 
+    // log the response
+    console.log(oData);
+    // assign the events array to a variable for manipulation
     var allevents = oData.events.event;
+    // log the array
     console.log(allevents);
+    // empty the div of any previously built band cards
     $("#cardplace").empty();
+    // starting at the index of 0 and for all the events in the array, do the following
     for ( var i=0; i < allevents.length; i++) {
+      // log the current index event you are at
       console.log(allevents[i]);
-      var eventcard = $("<div class='card eventful'>")
-      var artistName = $("<h1 class='card-title'>").text(allevents[i].title);
-      if (allevents[i].image == null){ 
-      var artistImage = $("<p>").text("no image avaliable");
+      // create a Semantic UI card and assign it to the ebandcard variable
+      var ebandcard = $("<div class=' ui card' id='artist-div'>");
+      // create a content div to append information to and assign it to eventcardcontent
+      var eventcardcontent = $("<div class='content eventful'>");
+      // create a header div to append information to and assign it artistName
+      var artistName = $("<div class='header'>");
+      // create a h1 element with the text of the artist title and assign it to artname
+      var artname = $("<h1>").text(allevents[i].title);
+      // append artname to artistName div 
+      $(artistName).append(artname);
+      // if no image loaded
+      if (allevents[i].image == null){
+        //  then create a paragraph element with the text "no image avaliable" and assign it to the aristImage variable
+          var artistImage = $("<p>").text("no image avaliable");
+        // if there is an image...
       } else {
-        var artistImage = $("<img class='card-img-top'>").attr("src", allevents[i].image.medium.url);
+        // create an image element with the source of the url from the response
+        var artistImage = $("<img class='image'>").attr("src", allevents[i].image.medium.url);
       }
+      // create paragraph elements with the text of the venue, city, region, and date of the event
       var venue = $("<p>").text(allevents[i].venue_name);
       var vlocal = $("<p>").text(allevents[i].city_name + " ," + allevents[i].region_name + " ," + allevents[i].country_name);
       var date = $("<p>").text(allevents[i].start_time);
+      // log the eventful url
       console.log(allevents[i].url);
+      // create an anchor tag with the bands eventful url as the link,
+      // tickets as the text,
+      // and assign to link variable
       var link = $("<a>").attr("href", allevents[i].url);
       $(link).text("Tickets");
-      $(eventcard).append(artistName, artistImage, vlocal, link, venue, date);
-      $("#cardplace").append(eventcard);
+      // append all information to content div
+      $(eventcardcontent).append(artistName, vlocal, link, venue, date);
+      // append image and content to card div
+      $(ebandcard).append(artistImage, eventcardcontent);
+      // append eventful band card div to DOM
+      $("#cardplace").append(ebandcard);
     }
   });
 }
-function searchBandsInTown(artist) {
-  if (artist == "") {
-    console.log("no artist")
+/////////// Function for calling BandsInTown API //////////
+function searchBandsInTown(inputArtist) {
+  // if the user input is equal to blank, log no input given and end function
+  if (inputArtist == "") {
+    console.log("no input given")
     return false;
   }
-
-  // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
-  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+  var queryURL = "https://rest.bandsintown.com/artists/" + inputArtist + "?app_id=codingbootcamp";
+////////// AJAX API call //////////
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-
-    // Printing the entire object to console
+////////// Response Manipulation //////////
+    // log the response
     console.log(response);
+     // empty the div of any previously built band cards
     $("#cardplace").empty();
-    // Constructing HTML containing the artist information
-    
     var artistName = response.name;
-    var artistURL = $("<div class=content'").append($("<a class='header'>").attr("href", response.url).append(artistName));
-    var artistImage = $("<div class='image'").append($("<img>").attr("src", response.thumb_url))
-    var goToArtist = $("<div class='description'").append($("<a>").attr("href", response.url).text("See Tour Dates"));
+    var emptybio = $("<div id='biog'>");
+    var cardcontent = $("<div class='content'>");
+    var artistlink = $("<a class='header'>");
+    $(artistlink).attr("href", response.url);
+    $(artistlink).text(artistName);
+    $(cardcontent).append(artistlink);
+    var artistImage = $("<div class='image'>").append($("<img>").attr("src", response.thumb_url))
+    var description = $("<div class='description'>");
+    var tdspan = $("<span>");
+    $(tdspan).append($("<a>").attr("href", response.url).text("See Tour Dates"));
+    $(description).append(tdspan);
+    $(cardcontent).append(description);
+    
     console.log(artistName);
-    console.log(artistURL);
+    console.log(cardcontent);
     console.log(artistImage);
-    // Empty the contents of the artist-div, append the new artist content
-    var bandcard = $("<div class=' ui card' id='artist-div'>")
-    $(bandcard).append(artistURL, artistImage, goToArtist);
+
+    var bandcard = $("<div class=' ui card' id='artist-div'>");
+    $(bandcard).append(artistImage, emptybio, cardcontent);
     $("#cardplace").append(bandcard);
   });
 }
 $(document).ready(function () {
   
   $("#artist").on("click", function () {
-    $("#dembutts").empty();
+    $("#dembutts").css("visibility", "hidden");
     $("#artist-input").css("visibility", "visible");
     $("#select-artist").css("visibility", "visible");
     
   });
   $("#city").on("click", function () {
-    $("#dembutts").empty();
+    $("#dembutts").css("visibility", "hidden");
     $("#where").css("visibility", "visible");
     $("#select-location").css("visibility", "visible");
     $("#keyword").css("visibility", "visible");
@@ -121,22 +181,39 @@ $(document).ready(function () {
   $("#select-location").on("click", function (event) {
     // Preventing the button from trying to submit the form
     event.preventDefault();
-    logo.animate({ height: "350px" });
-    // Storing the artist name
+    if (logovalue < 1) {
+      logo.animate({ 
+        top: "-=300px",
+        right: "-=600px",
+        width: "150px"
+    }, "slow");
+  }
+    logovalue = logovalue + 1;
+    // Storing the inputArtist name
     var keyword = $("#keyword").val().trim();
     where = $("#where").val().trim();
+    
     evdbapi(keyword, where);
   });
 
 
-  // Event handler for user clicking the select-artist button
+  // Event handler for user clicking the select-inputArtist button
   $("#select-artist").on("click", function (event) {
+
+    console.log(logovalue);
     // Preventing the button from trying to submit the form
     event.preventDefault();
+    if (logovalue < 1) {
     logo.animate({ 
-      left: "-=400px",
-      width: "200px"
+      top: "-=300px",
+      right: "-=600px",
+      width: "150px"
   }, "slow");
+}
+  logovalue = logovalue + 1;
+  var inputArtist = $("#artist-input").val().trim();
+  searchBandsInTown(inputArtist);
+   lastfm(inputArtist);
 
   });
 });
